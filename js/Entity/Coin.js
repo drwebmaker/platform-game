d17fbb649f3ac9d5ba9d7b36d7eab3b4b8d47b6b
Valera Abornev <dr.webmaker@gmail.com>
2015-07-23 10:20:22 +0300

    add test branch

--- js/Entity/Coin.js
+++ js/Entity/Coin.js
@@ -1,9 +1,11 @@
 var Vector = require('../World/Vector');
 
 function Coin(pos) {
+
   this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
   this.size = new Vector(0.6, 0.6);
   this.wobble = Math.random() * Math.PI * 2;
+
 }
 Coin.prototype.type = 'coin';
 
--- js/World/Level.js
+++ js/World/Level.js
@@ -2,37 +2,43 @@ var actorChars = require('../Helpers/actorChars.js');
 var Vector = require('./Vector');
 
 function Level(plan) {
-  this.width = plan[0].length;
-  this.height = plan.length;
-  this.grid = [];
-  this.actors = [];
+  if(Object.prototype.toString.call(plan).toUpperCase() === '[OBJECT ARRAY]') {
+    this.width = plan[0].length;
+    this.height = plan.length;
+    this.grid = [];
+    this.actors = [];
 
-  for (var y = 0; y < this.height; y++) {
-    var line = plan[y], gridLine = [];
-    for (var x = 0; x < this.width; x++) {
-      var ch = line[x], fieldType = null;
-      var Actor = actorChars[ch];
-      if (Actor) {
-        this.actors.push(new Actor(new Vector(x, y), ch));
-      } else if (ch == 'x') {
-        fieldType = 'wall';
-      } else if (ch == '!') {
-        fieldType = 'lava';
+    for (var y = 0; y < this.height; y++) {
+      var line = plan[y], gridLine = [];
+      for (var x = 0; x < this.width; x++) {
+        var ch = line[x], fieldType = null;
+        var Actor = actorChars[ch];
+        if (Actor) {
+          this.actors.push(new Actor(new Vector(x, y), ch));
+        } else if (ch == 'x') {
+          fieldType = 'wall';
+        } else if (ch == '!') {
+          fieldType = 'lava';
+        }
+        gridLine.push(fieldType);
       }
-      gridLine.push(fieldType);
+      this.grid.push(gridLine);
     }
-    this.grid.push(gridLine);
+
+    this.player = this.actors.filter(function(actor) {
+      return actor.type == 'player';
+    })[0];
+    this.status = this.finishDelay = null;
+  } else {
+    throw new Error('Incorrect input data format');
   }
 
-  this.player = this.actors.filter(function(actor) {
-    return actor.type == 'player';
-  })[0];
-  this.status = this.finishDelay = null;
 }
 
 Level.prototype.isFinished = function() {
   return this.status != null && this.finishDelay < 0;
 };
+
 
 
 Level.prototype.obstacleAt = function(pos, size) {
@@ -68,6 +74,10 @@ Level.prototype.actorAt = function(actor) {
 var maxStep = 0.05;
 
 Level.prototype.animate = function(step, keys) {
+
+  //console.log(step);
+  //console.log(keys);
+
   if (this.status != null)
     this.finishDelay -= step;
 
@@ -98,3 +108,6 @@ Level.prototype.playerTouched = function(type, actor) {
 };
 
 module.exports = Level;
+
+
+
