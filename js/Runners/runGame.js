0527b563627d1e42a74861bf4d580ff5cdf57e16
Valera Abornev <dr.webmaker@gmail.com>
2015-07-24 16:51:45 +0300

    showing Lives and Level

--- js/Runners/runGame.js
+++ js/Runners/runGame.js
@@ -4,13 +4,20 @@ var elt = require('../Helpers/elt');
 
 function runGame(plans, Display) {
   var lives  = 3;
+  var livesSpan = document.querySelector('#lives');
+  var levelSpan = document.querySelector('#level');
+
   function startLevel(n) {
+    livesSpan.textContent = lives;
+    levelSpan.textContent = n+1;
+
     if (lives > 0) {
       runLevel(new Level(plans[n]), Display, function (status) {
         if (status == 'lost')
           startLevel(n);
-        else if (n < plans.length - 1)
+        else if (n < plans.length - 1) {
           startLevel(n + 1);
+        }
         else {
           var youWin = document.body.appendChild(elt('div', 'game-over')).appendChild(elt('div', 'tableBlock'));
           var text = document.createTextNode('YOU WIN');
@@ -22,9 +29,16 @@ function runGame(plans, Display) {
       var youLost = document.body.appendChild(elt('div', 'game-over')).appendChild(elt('div', 'tableBlock'));
       var text = document.createTextNode('GAME OVER');
       youLost.appendChild(text);
+      addEventListener('keydown',function(event){
+        console.log(event.which);
+      });
     }
   }
   startLevel(0);
 }
 
 module.exports = runGame;
+
+addEventListener('keydown',function(event){
+  console.log(event.which);
+});
